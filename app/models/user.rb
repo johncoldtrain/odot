1cc18b3365837c194902d6e62b3e4e7991d7d28f
AR iMac <alexromanillos@Alexs-iMac.local>
2015-03-17 11:49:37 -0600

    Add user method for creating todo list tutorial

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,16 @@ class User < ActiveRecord::Base
 	end
 
 
+	def create_default_lists
+		tutorial = todo_lists.find_or_create_by(title: "ODOT Tutorial") ### <---- FIND OR CREATE METHOD ! 
+		
+		tutorial.todo_items.find_or_create_by(content: "Add a todo list or item by clicking the \"+\" button at the top.")
+	  tutorial.todo_items.find_or_create_by(content: "The numbers next to a list indicate how many items it has.")
+	  tutorial.todo_items.find_or_create_by(content: "Clicking or tapping the list title brings up list items.")
+	  tutorial.todo_items.find_or_create_by(content: "Clicking or tapping the list title again gives you more options.")
+	  tutorial.todo_items.find_or_create_by(content: "Clicking or tapping a checkmark next to an item marks it complete.")
+	  tutorial.todo_items.find_or_create_by(content: "Clicking or tapping it again marks it incomplete.")
+	  tutorial.todo_items.find_or_create_by(content: "Clicking or tapping the item lets you edit or delete it.")
+	end
+
 end
